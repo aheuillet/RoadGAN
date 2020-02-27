0c1e477d827e53bd36c255a4c14caeb54be44d05
Alexandre Heuillet <ale.heuillet@gmail.com>
2020-02-27 16:04:47 +0100

    fixed utils

--- utils.py
+++ utils.py
@@ -16,7 +16,7 @@ def decompose_video(video_path):
     print("Reading video file...")
     with tqdm() as pbar:
         while success:
-            cv2.imwrite(vid_name + "/_frame%d.jpg" % count, image)     # save frame as JPEG file      
+            cv2.imwrite(vid_name + "/%d.jpg" % count, image)     # save frame as JPEG file      
             success,image = vidcap.read()
             count += 1
             pbar.update()
@@ -24,15 +24,20 @@ def decompose_video(video_path):
 def recompose_video(img_dir_path, save_path):
     sample_path = random.choice(os.listdir(img_dir_path))
     sample = np.array(cv2.imread(os.path.join(img_dir_path, sample_path)))
-    fourcc = cv2.VideoWriter_fourcc(*'MP4V') 
-    writer = cv2.VideoWriter(save_path, fourcc, 30, (1280, 720))
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    print(sample.shape[0])
+    print(sample.shape[1]) 
+    writer = cv2.VideoWriter(save_path, fourcc, 25, (sample.shape[1], sample.shape[0]))
     for root, dirs, files in os.walk(img_dir_path):
         with tqdm(total=len(files)) as pbar:
-            for f in files.sort(key=int):
-                print(f)
-                img = cv2.imread(f)
+            files.sort(key=lambda x: int(x.split(".")[0]))
+            for f in files:
+                img = cv2.imread(os.path.join(img_dir_path, f))
                 writer.write(img)
+                pbar.update()
+    writer.release()
 
 
 
+#decompose_video("sample.mp4")
 recompose_video("./sample", "recomposed.mp4")
