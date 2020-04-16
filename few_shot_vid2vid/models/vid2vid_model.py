a7eb25e594b7bcef3431f9f4f8b5a32d5978d658
Alexandre Heuillet <ale.heuillet@gmail.com>
2020-04-16 15:16:54 +0200

    debug

--- few_shot_vid2vid/models/vid2vid_model.py
+++ few_shot_vid2vid/models/vid2vid_model.py
@@ -171,8 +171,8 @@ class Vid2VidModel(BaseModel):
         for i in range(N):
             n = np.random.normal()
             lipschitz_value = self.forward_discriminator(tgt_labels, tgt_image, ref_labels, ref_images, prev_label=prevs[0], prev_image=prevs[1], prev_t=z[i])
-            lipschitz_value = [loss.cpu().clone().numpy() for loss in lipschitz_value]
-            print("Lipshitz: ", -np.log(z0)-logit(np.array(lipschitz_value)), flush=True) #Convert each torch loss tensor into a numpy array
+            lipschitz_value = [loss.mean().cpu().clone().numpy() for loss in lipschitz_value]
+            print("Lipshitz: ", lipschitz_value, flush=True) #Convert each torch loss tensor into a numpy array
             z.append(z[i] - (eps/2)*np.gradient((-np.log(z0)-logit(np.array(lipschitz_value)))) + np.sqrt(eps*n)) #Langevin equation to sample the latent space using an EBM
         return z.pop()
 
